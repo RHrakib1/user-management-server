954a7a49c5869c3f69abafd37d12c0eb117d42d1
rakib hasan <rakibhasanmd457@gmail.com>
2025-07-29 06:25:15 +0600

    server management system test and successfuly done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+
+app.get('/', (req, res) => {
+    console.log('user managment server Runing');
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port is: ${port}`);
+})
