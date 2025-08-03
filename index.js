134b03dfa62ca77f7f78e67ce3eba66c822996e2
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-03 12:58:22 +0600

    add new pc

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express')
+const cros =require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
+//midelware
+app.use(cros())
+
+
+const empoloyes = [
+    {id:1,name:'rakib',email:'rakib@gmil.com'},
+    {id:2,name:'raju',email:'raju@gmil.com'},
+    {id:3,name:'alamin',email:'alamin@gmil.com'},
+]
 
 
 app.get('/', (req, res) => {
     console.log('user managment server Runing');
+     res.send('Server is running!');
+})
+app.get('/user',(req,res)=>{
+    res.send(empoloyes)
 })
 
-app.get('/', (req, res) => {
-    console.log('user managment server Runing');
-})
 
 app.listen(port, () => {
-    console.log(`server is running on port is: ${port}`);
-})
+    console.log(`Server is listening on port ${port}`);
+});
+
+
+
 
