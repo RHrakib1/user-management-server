82b127e482b82540036b63f042239b7b03f49f8f
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-03 09:35:18 +0600

    add

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     console.log('user managment server Runing');
 })
 
+app.get('/', (req, res) => {
+    console.log('user managment server Runing');
+})
+
 app.listen(port, () => {
     console.log(`server is running on port is: ${port}`);
 })
+
